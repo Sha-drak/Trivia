94adf1e116ec1708817b6afb97785d25978160a4
Alale Shadrach <sharonalale7@gmail.com>
2024-05-20 00:25:34 +0000

    Added a score board

--- trivia.js
+++ trivia.js
@@ -1,21 +1,27 @@
-//Selecting the html elements to be used 
-const score = document.querySelector('#sc');
+//Selecting the html elements to be used
+const score = document.querySelector("#sc");
 const feedback = document.getElementById("fr-feedback");
 const responseField = document.getElementById("fr-response");
+const comment = document.querySelector('#comment h3'); 
 
 // Set the initial score to zero
-score.innerHTML = "0"; 
+score.innerHTML = "0";
+
+// Creating an array for positive feedback
+positive =['Kudos!','Great job','You got it','You make this look easy!','Bravo!',];
+
+// Creating an array for negative feedback
+negative =['Oops!','Try Again!','Not Quite It','You Missed!','Give Another Shot',];
 
 // Creating a function that will check answers for multiple choice questions
-function checkAnswer(button, isCorrect, feedbackId){ //button = specifies the button pressed, isCorrect = checks if the button is correct, feedbackId = gives "correct" or "incorrect" feedback to the user
-const feedback = document.getElementById(feedbackId);
+function checkAnswer(button, isCorrect, feedbackId) {
+  const feedback = document.getElementById(feedbackId);
   if (isCorrect) {
     button.style.backgroundColor = "green";
     feedback.textContent = "Correct!";
     feedback.style.color = "green";
-    score.innerHTML = parseInt(score.innerHTML) + 1 ; 
-  } 
-  else {
+    score.innerHTML = parseInt(score.innerHTML) + 1;
+  } else {
     button.style.backgroundColor = "red";
     feedback.textContent = "Incorrect";
     feedback.style.color = "red";
@@ -30,15 +36,21 @@ const feedback = document.getElementById(feedbackId);
 // Creating a function that will check answers for free response questions
 function checkFreeResponse() {
   const userResponse = responseField.value.toLowerCase().trim(); //convert user input to lower case string
+
+  // Turn input field green when input is correct
   if (userResponse === "pacific") {
-    score.innerHTML = parseInt(score.innerHTML) + 1 ; 
+    score.innerHTML = parseInt(score.innerHTML) + 1;
     responseField.style.backgroundColor = "green";
     feedback.textContent = "Correct!";
     feedback.style.color = "green";
+
+    // Turn input field red when input is wrong
   } else {
     responseField.style.backgroundColor = "red";
     feedback.textContent = "Incorrect";
     feedback.style.color = "red";
+
+    // Set color back to default
     setTimeout(() => {
       responseField.style.backgroundColor = "";
       feedback.textContent = "";
